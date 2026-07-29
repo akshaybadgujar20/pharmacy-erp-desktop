a6e841654d87b78dc8e2bfa0b99d85609906df08
Akshay <asb1945@gmail.com>
2026-07-29 19:53:52 +0530

    chart js added

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,6 +1,14 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
 import {ColDef} from 'ag-grid-community';
 import {AgGridAngular} from 'ag-grid-angular';
+
+import {
+  Chart,
+  ChartConfiguration,
+  registerables
+} from 'chart.js';
+
+Chart.register(...registerables);
 
 @Component({
   selector: 'app-dashboard',
@@ -10,7 +18,7 @@ import {AgGridAngular} from 'ag-grid-angular';
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
 })
-export class DashboardComponent {
+export class DashboardComponent implements AfterViewInit {
   columnDefs: ColDef[] = [
     { field: 'id' },
     { field: 'name' },
@@ -22,4 +30,50 @@ export class DashboardComponent {
     { id: 2, name: 'Jane', city: 'Manchester' },
     { id: 3, name: 'Alex', city: 'Birmingham' }
   ];
+
+  @ViewChild('salesChart1')
+  chartCanvas1!: ElementRef<HTMLCanvasElement>;
+
+  @ViewChild('salesChart2')
+  chartCanvas2!: ElementRef<HTMLCanvasElement>;
+
+  @ViewChild('salesChart3')
+  chartCanvas3!: ElementRef<HTMLCanvasElement>;
+
+  chart!: Chart;
+
+  ngAfterViewInit(): void {
+
+    const config: ChartConfiguration<'bar'> = {
+      type: 'bar',
+      data: {
+        labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May'],
+        datasets: [
+          {
+            label: 'Sales',
+            data: [120, 180, 140, 220, 170]
+          }
+        ]
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false
+      }
+    };
+
+    this.chart = new Chart(
+      this.chartCanvas1.nativeElement,
+      config
+    );
+
+    this.chart = new Chart(
+      this.chartCanvas2.nativeElement,
+      config
+    );
+
+    this.chart = new Chart(
+      this.chartCanvas3.nativeElement,
+      config
+    );
+  }
 }
